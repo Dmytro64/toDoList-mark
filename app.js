f4c2120a4450e027220522387baab91d989c5176
Дмитро Тимрієнко <60476697+Dmytro64@users.noreply.github.com>
2022-08-24 23:52:56 +0300

    refresh

---
+++ app.js
@@ -0,0 +1,40 @@
+const addTaskBtn = document.getElementById('add-task-btn');
+const deskTaskInput = document.getElementById('description-task');
+const todosWrapper = document.querySelector('.todos-wrapper');
+
+let tasks;
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
+
+function Task(description) {
+   this.description = description;
+   this.compleated = false;
+};
+
+const createTemplate = (task, index) => {
+   return `
+      <div class="todo-item">
+      <div class="description">${task.description}</div>
+      <div class="buttons">
+         <input class="btn-complete" type="checkbox">
+         <button class="btn-delete">Видалити</button>
+      </div>
+      </div>
+   `
+};
+const fillHtmlList = () => {
+   todosWrapper.innerHTML = "";
+   if (tasks.length > 0) {
+      tasks.forEach((item, index) => {
+         todosWrapper.innerHTML += createTemplate(item, index);
+      })
+   }
+};
+const updateLocal = () => {
+   localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+addTaskBtn.addEventListener('click', () => {
+   tasks.push(new Task(deskTaskInput.value));
+   updateLocal();
+   fillHtmlList();
+});
